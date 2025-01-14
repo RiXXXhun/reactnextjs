9f872fa1bcfe63481928fb82acaa9d9c0440b76a
RiXXXhun <nagyeperjesirichard@gmail.com>
2025-01-14 02:52:08 +0100

    asd

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,16 @@
+
+import Navbarmenu from "@/components/Navbarmenu";
+import { Col, Row } from "react-bootstrap";
+
+export default function Page() {
+    
+
+    return (
+        <Row>
+            <Col xs={10}>
+                <Navbarmenu />
+            </Col>
+        </Row>
+        
+    )
+}
