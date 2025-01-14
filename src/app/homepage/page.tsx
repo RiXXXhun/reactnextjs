2c17058b9c77729ec17ef12fc067fadec0162894
RiXXXhun <nagyeperjesirichard@gmail.com>
2025-01-14 17:09:53 +0100

    ...

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,16 +1,28 @@
 
+import ResponsiveAppBar from "@/components/Materialuinavbar";
 import Navbarmenu from "@/components/Navbarmenu";
+import Navbarmenu2 from "@/components/Navbarmenu2";
 import { Col, Row } from "react-bootstrap";
 
 export default function Page() {
     
 
     return (
-        <Row>
-            <Col xs={10}>
+        <><Row>
+            <Col>
                 <Navbarmenu />
             </Col>
         </Row>
+        <Row>
+            <Col>
+             <Navbarmenu2 />
+             <ResponsiveAppBar />
+            </Col>
+        </Row>
+        
+        
+        </>
+
         
     )
 }
