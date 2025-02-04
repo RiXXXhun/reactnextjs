180efb031c505184708048bee9e71ce34b36c90f
RiXXXhun <nagyeperjesirichard@gmail.com>
2025-02-04 02:23:02 +0100

    fixedtohomepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter(); 
 
   const handleClick = () => {
-    router.push("/homepage");  
+    window.location.href = '/homepage'; 
   };
 
   return (
