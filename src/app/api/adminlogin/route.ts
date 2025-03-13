a07ae9a2d9b9a9bac8710d7a7584557c89eb0b42
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2025-03-13 19:12:44 +0100

    Seed files and adminlogin bcrypt

--- src/app/api/adminlogin/route.ts
+++ src/app/api/adminlogin/route.ts
@@ -1,31 +1,40 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   const { username, password } = await request.json();
 
-
   const admin = await prisma.admin.findUnique({
     where: { username },
   });
 
-  if (admin && admin.password === password) {
-
-    const supportData = await prisma.support.findMany();
-
-    return new Response(
-      JSON.stringify({
-        success: true,
-        message: "Login successful",
-        admin,
-        supportData, 
-      })
-    );
-  } else {
+  if (!admin) {
     return new Response(
       JSON.stringify({ success: false, message: "Incorrect username or password" }),
       { status: 400 }
     );
   }
+
+  // 🔹 Helyes bcrypt összehasonlítás
+  const passwordMatch = await bcrypt.compare(password, admin.password);
+
+  if (!passwordMatch) {
+    return new Response(
+      JSON.stringify({ success: false, message: "Incorrect username or password" }),
+      { status: 400 }
+    );
+  }
+
+  const supportData = await prisma.support.findMany();
+
+  return new Response(
+    JSON.stringify({
+      success: true,
+      message: "Login successful",
+      admin,
+      supportData, 
+    })
+  );
 }
