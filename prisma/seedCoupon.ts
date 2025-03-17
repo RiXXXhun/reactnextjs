1e03d65510ae07150ad721f8e7fba32708e4f112
zotya05 <100133701+zotya05@users.noreply.github.com>
2025-03-17 15:04:15 +0100

    users

--- prisma/seedCoupon.ts
+++ prisma/seedCoupon.ts
@@ -1,3 +1,23 @@
-import { PrismaClient } from "@prisma/client";
-
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+async function main() {
+  const coupons = [
+    { code: 'DISCOUNT10', discount: 10, expiration: new Date('2025-12-31') },
+    { code: 'DISCOUNT20', discount: 20, expiration: new Date('2025-12-31') },
+    // További kuponok...
+  ];
+
+  for (const coupon of coupons) {
+    await prisma.coupon.create({ data: coupon });
+  }
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- prisma/seedUser.ts
+++ prisma/seedUser.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, Prisma } from '@prisma/client';
-const bcrypt = require('bcrypt');
-const faker= require('faker');
+import bcrypt from 'bcryptjs';
+import { faker } from '@faker-js/faker';
+
 
 const prisma = new PrismaClient();
 
@@ -8,7 +9,7 @@ const generateUsers = async (numUsers: number): Promise<Prisma.UserCreateInput[]
   const users: Prisma.UserCreateInput[] = [];
 
   for (let i = 0; i < numUsers; i++) {
-    const username: string = faker.name.findName();
+    const username: string = faker.person.fullName();
     const email: string = faker.internet.email();
     const password: string = await bcrypt.hash('passworddscdvxd123', 10); // Hashelt jelszó
     const securityQuestionAnswer: string = await bcrypt.hash('Mi a/volt beceneved', 10); // Hashelt biztonsági kérdés
@@ -33,7 +34,7 @@ const seedUsers = async () => {
     });
   }
 
-  console.log('Users seeded successfully');
+  console.log('Users seed kész');
 };
 
 seedUsers().catch((e) => {
