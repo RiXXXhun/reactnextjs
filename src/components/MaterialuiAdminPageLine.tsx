ac4e920ed2579964f8587114ea2086eb269cc607
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2025-03-22 14:39:25 +0100

    line change

--- src/components/MaterialuiAdminPageLine.tsx
+++ src/components/MaterialuiAdminPageLine.tsx
@@ -10,7 +10,7 @@ export default function Linebar() {
                 mb: "30px",
                 height: "30px",
                 width: "100%",
-                backgroundColor: "#161C27",
+                backgroundColor: "#1E2A47",
                 position: "relative",
             }}
         >
@@ -25,32 +25,7 @@ export default function Linebar() {
                     transform: "translateY(-50%)",
                 }}
             />
-
-            <Box
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "0%",
-                    height: "4px",
-                    width: "20%",
-                    backgroundColor: "#1E90FF",
-                    transform: "translateY(-50%)",
-                    animation: `moveLine 8s linear infinite`,
-                }}
-            />
-            <style jsx global>{`
-                @keyframes moveLine {
-                    0% {
-                        left: 0;
-                    }
-                    50% {
-                        left: 80%;
-                    }
-                    100% {
-                        left: 0%;
-                    }
-                }
-            `}</style>
+                
         </Box>
     );
 }
