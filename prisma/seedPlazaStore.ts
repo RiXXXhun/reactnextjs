7669d4ff70ee0bff03ddf948724b21330b68cd4d
zotya05 <100133701+zotya05@users.noreply.github.com>
2025-03-28 08:48:39 +0100

    adatok

---
+++ prisma/seedPlazaStore.ts
@@ -0,0 +1,43 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const plazastore = [
+    {name:"One" ,closigTime: "20:00",openingTime:"08:00"},
+    {name:"Konzolvilág" ,closigTime: "20:00",openingTime:"08:00"},
+    {name:"Bubu Tea/Ice'n'GO" ,closigTime: "20:00",openingTime:"08:00"},
+    {name:"Tescoma" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Lipóti" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Tezenis" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Calzedonia" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"BÁV zálog és ékszer" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Gold for you" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Cinema City" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Vision Express" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Vidanet" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Yettel" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Telekom" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"SportFactory" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Rossmann" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Retro" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Regio Játék" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Raiffeisen Bank" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Pizza Hut" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Pepco" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Libri" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"KFC" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"K&H" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"HajSzalon" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Devergo&friends" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Deichmann" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"CCC" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Budmil" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"BioTechUSA" ,closigTime: "20:00",openingTime:"08:00"},
+    {name:"BENU Gyógyszertár" ,closingTime: "20:00",openingTime:"08:00"},
+    {name:"Auchan" ,closingTime: "20:00",openingTime:"08:00",}
+
+];
+
+
+
+  
